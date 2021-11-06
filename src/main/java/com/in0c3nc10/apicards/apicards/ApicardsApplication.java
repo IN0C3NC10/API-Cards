57d4526d148a8e6cd10ce29c0a194a04d1c9ea04
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-11-06 19:29:08 -0300

    Starting APICards project

---
+++ src/main/java/com/in0c3nc10/apicards/apicards/ApicardsApplication.java
@@ -0,0 +1,13 @@
+package com.in0c3nc10.apicards.apicards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in0c3nc10/apicards/apicards/ApicardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in0c3nc10.apicards.apicards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
